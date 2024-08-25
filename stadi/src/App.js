e3f510ed42b401a0680b3e6831eec8b50058aa3c
catherinekimani <catherinekimani882@gmail.com>
2024-08-25 04:54:50 +0300

    Modify App.js

--- stadi/src/App.js
+++ stadi/src/App.js
@@ -5,6 +5,9 @@ import Register from "./components/Register";
 // import Navbar from "./components/Navbar";
 import LandingPage from "./pages/landingPage";
 import AudioLessonsPage from "./pages/lesson";
+import Video from "./pages/Video";
+import Articles from "./pages/Articles";
+
 import FeaturesPage from "./pages/featuresPage";
 import NotFound from "./components/NotFound";
 
@@ -15,6 +18,8 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/lessons" element={<AudioLessonsPage />} />
+        <Route path="/videos" element={<Video />} />
+        <Route path="/articles" element={<Articles />} />
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="*" element={<NotFound />} />
